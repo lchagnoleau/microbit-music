aae4fc341b1f5c35fdfba812f64ed425b8c8ce5f
Loïc Chagnoleau <furlings@pm.me>
2024-01-11 18:03:55 +0100

    play note

--- embedded_software/src/main.rs
+++ embedded_software/src/main.rs
@@ -3,30 +3,26 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
 use microbit::{
     board::Board,
-    hal::{prelude::*, Timer},
-    display::blocking::Display,
+    hal::{gpio::*, pwm::*, time::Hertz},
 };
+use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    let mut board = Board::take().unwrap();
-    let mut timer = Timer::new(board.TIMER0);
-    let mut display = Display::new(board.display_pins);
+    let board = Board::take().unwrap();
+    let speaker_pin = board.speaker_pin.into_push_pull_output(Level::High);
+    let speaker = Pwm::new(board.PWM0);
+    speaker
+        .set_output_pin(Channel::C0, speaker_pin.degrade())
+        .set_prescaler(Prescaler::Div16)
+        .set_period(Hertz(440))
+        .set_counter_mode(CounterMode::UpAndDown)
+        .enable();
 
-    let heart = [
-        [0, 1, 0, 1, 0],
-        [1, 0, 1, 0, 1],
-        [1, 0, 0, 0, 1],
-        [0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 0],
-    ];
+    // Configure 50% duty cycle (volulme = 1000%)
+    speaker.set_duty_on_common(speaker.max_duty() / 2);
 
-    loop {
-        display.show(&mut timer, heart, 1000);
-        display.clear();
-        timer.delay_ms(1000u32);
-    }
+    loop {}
 }
