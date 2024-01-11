0a71eb9940b67e7c310d15fb2e4b53bb18e2bf35
Loïc Chagnoleau <furlings@pm.me>
2024-01-11 14:08:07 +0100

    heart

--- embedded_software/src/main.rs
+++ embedded_software/src/main.rs
@@ -4,15 +4,29 @@
 
 use cortex_m_rt::entry;
 use panic_halt as _;
-use microbit::board::Board;
-use microbit::hal::prelude::*;
+use microbit::{
+    board::Board,
+    hal::{prelude::*, Timer},
+    display::blocking::Display,
+};
 
 #[entry]
 fn main() -> ! {
     let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
 
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
+    let heart = [
+        [0, 1, 0, 1, 0],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+    ];
 
-    loop {}
+    loop {
+        display.show(&mut timer, heart, 1000);
+        display.clear();
+        timer.delay_ms(1000u32);
+    }
 }
