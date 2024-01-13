ce29ddf42a642ce4cf926d4b59f997e6902b16a5
Loïc Chagnoleau <furlings@pm.me>
2024-01-13 19:07:39 +0100

    uart in other file

--- embedded_software/src/main.rs
+++ embedded_software/src/main.rs
@@ -3,40 +3,24 @@
 #![no_std]
 
 // mod speaker;
-// mod uart;
+mod serial;
 
-use core::fmt::Write;
 use cortex_m_rt::entry;
-use hal::{gpio, uarte, uarte::Uarte};
+use hal::gpio::{p0, p1};
 use nrf52833_hal as hal;
 use panic_halt as _;
 // use speaker::Speaker;
-// use uart::Uart;
+use serial::Serial;
 
 #[entry]
 fn main() -> ! {
     let pac = hal::pac::Peripherals::take().unwrap();
-    let p0 = gpio::p0::Parts::new(pac.P0);
-    let p1 = gpio::p1::Parts::new(pac.P1);
+    let p0 = p0::Parts::new(pac.P0);
+    let p1 = p1::Parts::new(pac.P1);
 
-    let txd = p0.p0_06.into_push_pull_output(gpio::Level::High).degrade();
-    let rxd = p1.p1_08.into_floating_input().degrade();
+    let mut serial = Serial::new(pac.UARTE0, p0.p0_06, p1.p1_08);
 
-    let uart_pins = uarte::Pins {
-        txd,
-        rxd,
-        cts: None,
-        rts: None,
-    };
-
-    let mut serial = Uarte::new(
-        pac.UARTE0,
-        uart_pins,
-        uarte::Parity::EXCLUDED,
-        uarte::Baudrate::BAUD115200,
-    );
-
-    write!(serial, "Hello, World!\r\n").unwrap();
+    serial.write(b"Hello, world!\r\n");
 
     loop {}
 }
